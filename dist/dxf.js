ab212e1162ef2695b3dd1fce08fb9c0e3d58fd70
KamTah <kamyar.taher@isochronic.com>
2022-10-24 10:33:44 +0200

    layer as color

--- dist/dxf.js
+++ dist/dxf.js
@@ -4388,6 +4388,9 @@ function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Sy
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
+// color is a string like '#ff0000' give a list
+var colorTable = ['#000000', '#00FF00', '#0000FF', '#FF0000', '#01FFFE', '#FFA6FE', '#FFDB66', '#006401', '#010067', '#95003A', '#007DB5', '#FF00F6', '#FFEEE8', '#774D00', '#90FB92', '#0076FF', '#D5FF00', '#FF937E', '#6A826C', '#FF029D', '#FE8900', '#7A4782', '#7E2DD2', '#85A900', '#FF0056', '#A42400', '#00AE7E', '#683D3B', '#BDC6FF', '#263400', '#BDD393', '#00B917', '#9E008E', '#001544', '#C28C9F', '#FF74A3', '#01D0FF', '#004754', '#E56FFE', '#788231', '#0E4CA1', '#91D0CB', '#BE9970', '#968AE8', '#BB8800', '#43002C', '#DEFF74', '#00FFC6', '#FFE502', '#620E00', '#008F9C', '#98FF52', '#7544B1', '#B500FF', '#00FF78', '#FF6E41', '#005F39', '#6B6882', '#5FAD4E', '#A75740', '#A5FFD2', '#FFB167', '#009BFF', '#E85EBE'];
+
 var addFlipXIfApplicable = function addFlipXIfApplicable(entity, _ref) {
   var bbox = _ref.bbox,
       element = _ref.element;
@@ -4659,11 +4662,9 @@ var bezier = function bezier(entity) {
 
 var textRend = function textRend(entity) {
   if (entity.type !== 'TEXT') {
-    // console.log('how the fuck did this happen?', entity.type)
     return 0;
   }
 
-  console.log(entity);
   var bbox = new _vecks.Box2();
   var element = "<text x=\"".concat(entity.x, "\" y=\"").concat(-entity.y, "\" font-size=\"").concat(entity.textHeight, "px\" transform=\"matrix(1,0,0,-1,0,0) rotate(").concat(-entity.rotation, "\n    ,").concat(entity.x, ",").concat(-entity.y, ")\">").concat(entity.string, "</text>");
   return (0, _transformBoundingBoxAndElement["default"])(bbox, element, entity.transforms);
@@ -4736,7 +4737,8 @@ var _default = function _default(parsed) {
         acc.bbox.expandByPoint(bbox.max);
       }
 
-      var fillingStyle = entity.type === 'TEXT' ? "fill=\"".concat((0, _rgbToColorAttribute["default"])(rgb), "\"") : "stroke=\"".concat((0, _rgbToColorAttribute["default"])(rgb), "\" fill=\"none\"");
+      var layerColor = entity.layer ? colorTable[entity.layer] : (0, _rgbToColorAttribute["default"])(rgb);
+      var fillingStyle = entity.type === 'TEXT' ? "fill=\"".concat((0, _rgbToColorAttribute["default"])(rgb), "\"") : "stroke=\"".concat(layerColor, "\" fill=\"none\"");
       acc.elements.push("<g ".concat(fillingStyle, " >").concat(element, "</g>"));
     }
 
--- src/toSVG.js
+++ src/toSVG.js
@@ -9,6 +9,74 @@ import rgbToColorAttribute from './util/rgbToColorAttribute'
 import toPiecewiseBezier, { multiplicity } from './util/toPiecewiseBezier'
 import transformBoundingBoxAndElement from './util/transformBoundingBoxAndElement'
 
+// color is a string like '#ff0000' give a list
+const colorTable = [
+  '#000000',
+  '#00FF00',
+  '#0000FF',
+  '#FF0000',
+  '#01FFFE',
+  '#FFA6FE',
+  '#FFDB66',
+  '#006401',
+  '#010067',
+  '#95003A',
+  '#007DB5',
+  '#FF00F6',
+  '#FFEEE8',
+  '#774D00',
+  '#90FB92',
+  '#0076FF',
+  '#D5FF00',
+  '#FF937E',
+  '#6A826C',
+  '#FF029D',
+  '#FE8900',
+  '#7A4782',
+  '#7E2DD2',
+  '#85A900',
+  '#FF0056',
+  '#A42400',
+  '#00AE7E',
+  '#683D3B',
+  '#BDC6FF',
+  '#263400',
+  '#BDD393',
+  '#00B917',
+  '#9E008E',
+  '#001544',
+  '#C28C9F',
+  '#FF74A3',
+  '#01D0FF',
+  '#004754',
+  '#E56FFE',
+  '#788231',
+  '#0E4CA1',
+  '#91D0CB',
+  '#BE9970',
+  '#968AE8',
+  '#BB8800',
+  '#43002C',
+  '#DEFF74',
+  '#00FFC6',
+  '#FFE502',
+  '#620E00',
+  '#008F9C',
+  '#98FF52',
+  '#7544B1',
+  '#B500FF',
+  '#00FF78',
+  '#FF6E41',
+  '#005F39',
+  '#6B6882',
+  '#5FAD4E',
+  '#A75740',
+  '#A5FFD2',
+  '#FFB167',
+  '#009BFF',
+  '#E85EBE',
+]
+
 const addFlipXIfApplicable = (entity, { bbox, element }) => {
   if (entity.extrusionZ === -1) {
     return {
@@ -297,10 +365,8 @@ const bezier = (entity) => {
 
 const textRend = (entity) => {
   if (entity.type !== 'TEXT') {
-    // console.log('how the fuck did this happen?', entity.type)
     return 0
   }
-  console.log(entity)
   const bbox = new Box2()
   const element = `<text x="${entity.x}" y="${-entity.y}" font-size="${
     entity.textHeight
@@ -362,10 +428,14 @@ export default (parsed) => {
           acc.bbox.expandByPoint(bbox.min)
           acc.bbox.expandByPoint(bbox.max)
         }
+        const layerColor = entity.layer
+          ? colorTable[entity.layer]
+          : rgbToColorAttribute(rgb)
+
         const fillingStyle =
           entity.type === 'TEXT'
             ? `fill="${rgbToColorAttribute(rgb)}"`
-            : `stroke="${rgbToColorAttribute(rgb)}" fill="none"`
+            : `stroke="${layerColor}" fill="none"`
         acc.elements.push(`<g ${fillingStyle} >${element}</g>`)
       }
       return acc
